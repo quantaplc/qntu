508bc90108a1f3f9abdf68811721dec54077d9b0
longhg-ibl <longhg@blockchainlabs.asia>
2018-04-26 17:46:11 +0700

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -7,18 +7,6 @@ module.exports = {
 			gas: 4000000
 		},
 
-		ropsten: {
-			host: "172.104.183.123",
-			port: 8545,
-			network_id: "*"
-		},
-
-		mainnet: {
-			host: "178.79.173.30",
-			port: 8545,
-			network_id: "*"
-		},
-
 		solc: {
 			optimizer: {
 				enabled: true,
